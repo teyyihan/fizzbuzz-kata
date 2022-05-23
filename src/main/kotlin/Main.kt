6d240da97729ba01c866c93986b1c1883e119b8c
teyyihan.aksu <teyyihan.aksu@trendyol.com>
2022-05-23 11:38:12 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:.gradle/7.1/fileHashes/fileHashes.lock

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,36 @@
-fun main(args: Array<String>) {
-    val rnds = (0..10).random()
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+const val FIZZ = "Fizz"
+const val BUZZ = "Buzz"
+const val EMPTY_STRING = ""
+
+fun main(args: Array<String>) {
+    val number = getNumberFromUser()
+
+    val result = solveFizzBuzz(number)
+
+    println("Random number is $number, so it's $result")
 }
+
+fun solveFizzBuzz(number: Int): String {
+    var result = EMPTY_STRING
+
+    if(divisibleByThree(number))
+        result += FIZZ
+
+    if(divisibleByFive(number))
+        result += BUZZ
+
+    return result
+}
+
+fun getNumberFromUser(): Int {
+    val reader = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    return reader.nextInt()
+}
+
+fun divisibleByThree(number: Int): Boolean =  number % 3 == 0
+
+fun divisibleByFive(number: Int): Boolean =  number % 5 == 0
