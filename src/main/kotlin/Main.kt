6d412ad113a250b9e49c3b3ec4741b2b22b2e83a
teyyihan.aksu <teyyihan.aksu@trendyol.com>
2022-05-23 11:23:19 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val rnds = (0..10).random()
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
